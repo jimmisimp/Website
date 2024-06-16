110755d62070f3743f16cc3ebd8c32af9acbb9f6
jimmisimp <jimmis.imp@gmail.com>
2024-06-16 17:51:35 -0400

    pulsing button, new font, and prompt tweaks

--- src/components/TextGenerator.tsx
+++ src/components/TextGenerator.tsx
@@ -6,7 +6,8 @@ const openai = new OpenAI({apiKey: process.env.REACT_APP_OPENAIKEY, dangerouslyA
 async function generateText(setText) {
     const stream = await openai.chat.completions.create({
         model: "gpt-4o",
-        messages: [{ role: "user", content: "Write a another sentence like this one `is creating ai-powered experiences.`. Max of 12 words. Your output MUST start with `is` (lowercase) and be about the subject, Adam Yuras, a Senior UX and product design specialist from Philadelphia PA. He works at Comcast. It should describe what else he does. More about him from his profile: Designing, prototyping, and testing tools for customer-facing agents in the chat and voice space, for technicians, and retail associates with a focus on AI-enabled features. I'm a hands-on designer who prefers to explore solutions by developing prototypes in React and other frameworks. I'm a designer who thinks like a developer." }],
+        temperature: 1.1,
+        messages: [{ role: "user", content: "Write a another sentence like this one `is creating ai-powered experiences.`. Max of 12 words, try to keep it focused on a single skill, and try to avoid too many adjectives. Never imply that I'm a developer. Your output MUST start with `is` (lowercase) and be about the subject, Adam Yuras, a Senior UX and product design specialist from Philadelphia PA. He works at Comcast. It should describe what else he does. More about him from his profile: Designing, prototyping, and testing tools for customer-facing agents in the chat and voice space, for technicians, and retail associates with a focus on AI-enabled features. I'm a hands-on designer who prefers to explore solutions by developing prototypes in code. I'm a designer who thinks like a developer. I've helped develop the skills of those I work with. I'm a strong researcher, but I'm also business minded and know how to keep things moving and when we're wasting our time." }],
         stream: true,
     });
 
@@ -22,10 +23,12 @@ export const TextGenerator:React.FC = () => {
 
     return (
         <div>
-			<div>
+			<div className='generated-text'>
                 {text}
             </div>
-			<button className="main-button" onClick={()=> generateText(setText)}>What else?</button>
+			<button className="main-button pulse" onClick={()=> generateText(setText)}>What else?
+                <div className='pulse'/>
+            </button>
 		</div>
     )
 }
