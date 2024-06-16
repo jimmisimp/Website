a650e93508d923b7af7d2d3cf48ec2f49d92a6f1
jimmisimp <jimmis.imp@gmail.com>
2024-06-15 22:51:21 -0400

    favicon + prompt tweaks

--- src/components/TextGenerator.tsx
+++ src/components/TextGenerator.tsx
@@ -6,7 +6,7 @@ const openai = new OpenAI({apiKey: process.env.REACT_APP_OPENAIKEY, dangerouslyA
 async function generateText(setText) {
     const stream = await openai.chat.completions.create({
         model: "gpt-4o",
-        messages: [{ role: "user", content: "Write a another sentence like this one `is creating ai-powered experiences.`. Your output MUST start with `is` (lowercase) and be about the subject, Adam Yuras, a UX and product design specialist. It should describe what else he does." }],
+        messages: [{ role: "user", content: "Write a another sentence like this one `is creating ai-powered experiences.`. Max of 12 words. Your output MUST start with `is` (lowercase) and be about the subject, Adam Yuras, a Senior UX and product design specialist from Philadelphia PA. He works at Comcast. It should describe what else he does. More about him from his profile: Designing, prototyping, and testing tools for customer-facing agents in the chat and voice space, for technicians, and retail associates with a focus on AI-enabled features. I'm a hands-on designer who prefers to explore solutions by developing prototypes in React and other frameworks. I'm a designer who thinks like a developer." }],
         stream: true,
     });
 
